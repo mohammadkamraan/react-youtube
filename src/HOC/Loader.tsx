b390dce87c7e63e732844a29048b081a1c1d9af2
mohammadkamraan <kamyzenon@gmail.com>
2023-07-11 12:33:16 +0330

    Fixed code issues via data binding with hook

--- src/HOC/Loader.tsx
+++ src/HOC/Loader.tsx
@@ -2,14 +2,10 @@ import { ReactNode } from "react";
 
 import useHttpClient from "../hooks/useHttpClient";
 
-import {
-  IHttpClientOptions,
-  IRequestsHandlerParameter,
-} from "../utils/httpClient";
+import { IRequestsHandlerParameter } from "../utils/httpClient";
 
 interface ILoaderComponent {
-  request?: IHttpClientOptions;
-  requests?: IRequestsHandlerParameter;
+  requests: IRequestsHandlerParameter;
   errorElement: any;
   successElement: any;
   loadingElement: ReactNode;
@@ -18,28 +14,20 @@ interface ILoaderComponent {
 const Loader =
   (Component: any) =>
   ({
-    request,
     requests,
     successElement: SuccessElement,
     errorElement: ErrorElement,
     loadingElement,
   }: ILoaderComponent) => {
-    const [loading, data, error, loader, loaders] = useHttpClient(
-      request,
-      requests
-    );
+    const [loading, data, error, loader] = useHttpClient(requests);
 
     return (
       <Component
         data={data}
         error={error}
         loading={loading}
-        successElement={
-          <SuccessElement data={data} loader={loader} loaders={loaders} />
-        }
-        errorElement={
-          <ErrorElement error={error} loader={loader} loaders={loaders} />
-        }
+        successElement={<SuccessElement data={data} loader={loader} />}
+        errorElement={<ErrorElement error={error} loader={loader} />}
         loadingElement={loadingElement}
       />
     );
