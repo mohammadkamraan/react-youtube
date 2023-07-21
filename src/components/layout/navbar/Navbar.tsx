9bff1acd1d118a1bd9aaafda9fee93a51e825715
mohammadkamraan <kamyzenon@gmail.com>
2023-07-21 23:09:10 +0330

    Changed file name of navbar scss

--- src/components/layout/navbar/Navbar.tsx
+++ src/components/layout/navbar/Navbar.tsx
@@ -3,12 +3,11 @@ import { FC, memo } from "react";
 import NavbarBrand from "../../UI/navbar-brand/NavbarBrand";
 import NavbarOptions from "../../UI/navbar-options/NavbarOptions";
 import SearchForm from "../../UI/search-form/SearchForm";
-
-import styles from "./navbar.module.scss";
+import styles from "./navbar-styles.module.scss";
 
 const Navbar: FC = () => {
   return (
-    <nav className={styles.navbar}>
+    <nav className={styles["navbar"]}>
       <NavbarBrand />
       <SearchForm />
       <NavbarOptions />
